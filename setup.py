1ac1fcac4faca07a3de819c72f1e29d8c9dd39b0
Shubham Ugare <shubhamdugare@gmail.com>
2024-04-16 15:08:28 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,10 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+# Read the content of the requirements.txt file	
+with open('requirements.txt', 'r', encoding='utf-8') as f:	
+    requirements = f.read().splitlines()
+
 setuptools.setup(
     name="nn_verify",
     version="0.1",
@@ -13,6 +17,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/shubhamugare/nn_verify",
+    install_requires=requirements,
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
